d8754211f0802c3c8f2ec1d748d3e31c527f82c5
t@nm@y <writetotkpatra@gmail.com>
2018-09-24 22:57:48 +0530

    initial commit

---
+++ src/main/java/com/learn/gatling/tanmay/TryGatling/TryGatlingApplication.java
@@ -0,0 +1,12 @@
+package com.learn.gatling.tanmay.TryGatling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryGatlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryGatlingApplication.class, args);
+	}
+}
